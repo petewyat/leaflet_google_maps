5548c514b5322473a8aa800d7fe97716f94fe36f
petewyat <petewyat@isu.edu>
2021-03-25 11:29:22 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1vwFUIykP7IpmLQ_8Yw5NYqQPLdzmxN_qnA_xB5X1ZF4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gtujQsBOc3HOidUh10IronH1yj1DhKTYRxoXnaBo-Ls/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
